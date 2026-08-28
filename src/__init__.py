f97dd70367411b8548f5be38e0a90b622e324a8a
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-08-28 18:29:11 +0800

    feat: add read input json file

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,2 @@
 def main() -> None:
     print("Hello from call-me-maybe!")
-
-if __name__ == "__main__":
-    main()
---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+import json
+
+with open('data/input/function_calling_tests.json') as f:
+    data = json.load(f);
+    print(data)
+
+with open('data/input/functions_definition.json') as f:
+    data = json.load(f);
+    print(data)
+
+def main() -> None:
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
