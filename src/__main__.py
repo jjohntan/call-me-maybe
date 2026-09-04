9081e241f9dbc01dd8f826fed9bad1c712f90daf
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-09-04 17:20:35 +0800

    feat: add torch and transformers dependencies and implement encode method from Small_LLM_Model
    - import small llm model

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 import json
+from llm_sdk.llm_sdk import Small_LLM_Model
 
 with open('data/input/function_calling_tests.json') as f:
     data = json.load(f)
@@ -7,6 +8,12 @@ with open('data/input/function_calling_tests.json') as f:
 with open('data/input/functions_definition.json') as f:
     data = json.load(f)
     print(data)
+
+# instantiate an object
+model = Small_LLM_Model("Qwen/Qwen3-0.6B")
+# call encode method of the object
+input_ids = model.encode("Hello")
+print(input_ids)
 
 def main() -> None:
     print("Hello")
