1454c9ff0d69e13516b425f263c410fed0e9a32b
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-09-04 15:00:33 +0800

    fix: force project not to built and installed into the project environment
    project can run without uv default package module

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,11 @@
 import json
 
 with open('data/input/function_calling_tests.json') as f:
-    data = json.load(f);
+    data = json.load(f)
     print(data)
 
 with open('data/input/functions_definition.json') as f:
-    data = json.load(f);
+    data = json.load(f)
     print(data)
 
 def main() -> None:
